23ec5ccbd9f0c9539f26995464f363435cc7e20e
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-09 15:40:46 +0200

    Did rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "<<< Cleaning Restaurant DB >>>"
+Restaurant.destroy_all
+puts "<<< Adding fake restaurants >>>"
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    category: categories.sample,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+  restaurant.save
+  review = Review.new(content: "good review", rating: 5, restaurant_id: restaurant.id)
+  review2 = Review.new(content: "bad review", rating: 2, restaurant_id: restaurant.id)
+  review.save
+  review2.save
+end
+
+puts "<<< Added #{Restaurant.count} fake restaurants >>>"
