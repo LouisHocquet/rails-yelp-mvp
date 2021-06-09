59ded9cc82ce13cf48b8ca98c3e1f50778c74100
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-09 16:09:33 +0200

    Improve validation on reviews rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :rating, :content, presence: true
-  validates :rating, numericality: true
-  validates :rating, inclusion: (0..5).to_a
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
